820fb3ec80b3b69bf60c1e23e7ca98a7cf724bc3
Indro Lie <Indrolie@gmail.com>
2018-09-12 11:58:11 +0700

    Enable delete modal

--- src/Modal/index.js
+++ src/Modal/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import ReactModal from 'react-modal-resizable-draggable';
+import React from 'react'
+import ReactModal from 'react-modal-resizable-draggable'
+import { Icon, Grid } from 'semantic-ui-react'
 
 import './index.css';
 
@@ -13,26 +14,23 @@ export default class MyModal extends React.Component {
     }
 
     deleteModal = async (index) => {
-        delete this.state.modals[index - 1]
+        delete this.state.modals[index]
+
+        await this.setState({
+            modals: this.state.modals
+        })
 
         console.log(this.state.modals)
     }
 
     addModal = async () => {
+        await this.state.modals.push('')
+
         await this.setState({
-            modals: this.state.modals.concat(
-                <ReactModal key={this.state.modals.length} initWidth={400} initHeight={200} isOpen={true}>
-                    <h3>My Modal</h3>
-                    <div className="body">
-                        <p>This is the modal's body.</p>
-                    </div>
-                    <button onClick={() => this.deleteModal(this.state.modals.length)} >
-                        Close modal
-                    </button>
-                </ReactModal>
-            )
+            modals: this.state.modals
         })
-        console.log(this.state.modals)
+
+        await console.log(this.state.modals)
     }
 
     render() {
@@ -41,7 +39,27 @@ export default class MyModal extends React.Component {
                 <button onClick={this.addModal} className='button-add'>
                     Add modal
                 </button>
-                {this.state.modals}
+                {this.state.modals.map((modal, index) => {
+                    return (
+                        <ReactModal key={index} initWidth={400} initHeight={200} isOpen={true}>
+                            <div className="content">
+                                <div className="header">
+                                    <Grid>
+                                        <Grid.Column floated='left' width={5}>
+                                            <label>Welcome</label>
+                                        </Grid.Column>
+                                        <Grid.Column floated='right' className='icon-close'>
+                                            <Icon name='close' onClick={() => this.deleteModal(index)} />
+                                        </Grid.Column>
+                                    </Grid>
+                                </div>
+                                <div className="body">
+                                    <p>Hello there...</p>
+                                </div>
+                            </div>
+                        </ReactModal>
+                    )
+                })}
             </div>
         );
     }
